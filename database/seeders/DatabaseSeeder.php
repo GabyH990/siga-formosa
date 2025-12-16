b6ea04ea7cc334fed035c4c3fae9ef54705f0b5c
Gaby <gaviheretichi@gmail.com>
2025-12-16 10:10:06 -0300

    chore(seeders): update careers and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
             CareersSeeder::class,
             UsersSeeder::class,
             CarrerasMateriasComisionesSeeder::class,
+            StudentsSeeder::class,
+
         ]);
     }
 }
